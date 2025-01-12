4b74c4bf0139ff9265dbe9ad51dd28d6451d82e6
Samruddhi2405 <samruddhinarkhede7293@gmail.com>
2025-01-12 13:52:19 +0530

    changes

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,29 +1,30 @@
-import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
-  
-  const apiKey = process.env.AIzaSyCSIVHPn7W1LDSkqbMhhp__YfpX7Sf3lgk;
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI("AIzaSyCSIVHPn7W1LDSkqbMhhp__YfpX7Sf3lgk");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "What is react js";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
   });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run(prompt) {
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-  
-    const result = await chatSession.sendMessage(prompt);
-    console.log(result.response.text());
-  }
-  
-export default run;
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+}
+
+export default run();
